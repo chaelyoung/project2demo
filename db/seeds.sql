5ce3de3fc9ba2b53278eb305c93eec37e759ddb8
vschaitu <vschaitu@gmail.com>
2018-06-27 21:28:34 -0500

    some changes??

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,18 @@
 # Sample User Account Seed
-INSERT INTO `sale_db`.`user_account` (`firstname`, `lastname`, `username`, `about`, `email`, `password`, `last_login`, `status`) VALUES ('Mark	', 'Chaffin', 'mchaffin', 'to graduate', 'mark.chaffin@outlook.com', 'Fungi', '01/01/2018', 'Enabled');
+INSERT INTO `sale_db`.`user_account` (`firstname`, `lastname`, `username`, `about`, `email`, `password`, `status`,`createdAt`, `updatedAt`) VALUES ('Mark	', 'Chaffin', 'mchaffin', 'to graduate', 'mark.chaffin@outlook.com', 'Fungi', 'Enabled','2018-07-16 09:00:00','2018-07-16 09:00:00');
 
 # Sample Sale Event Seed
-INSERT INTO `sale_db`.`sale_event` (`useraccount_id`, `address`, `city`, `state`, `zip`, `date`, `start_time`, `end_time`) VALUES ('1', '123 Anywhere Lane', 'Minneapolis', 'MN', '55406', '01/01/2018', '12:00:00', '17:00:00');
+INSERT INTO `sale_db`.`sale_event` (`useraccount_id`, `address`, `city`, `state`, `zip`, `date`, `start_time`, `end_time`,`comments`, `photo_url`,`createdAt`, `updatedAt`) VALUES ('1', '123 Anywhere Lane', 'Minneapolis', 'MN', '55406', '2018-07-16', '2018-07-16 09:00:00', '2018-07-16 12:00:00','some comments for testing', 'http://via.placeholder.com/350x150','2018-07-16 09:00:00','2018-07-16 09:00:00');
+
+INSERT INTO `sale_db`.`sale_event` (`useraccount_id`, `address`, `city`, `state`, `zip`, `date`, `start_time`, `end_time`,`comments`, `photo_url`,`createdAt`, `updatedAt`) VALUES ('1', '456 Anywhere Lane', 'Minneapolis', 'MN', '55406', '2018-07-16', '2018-07-16 09:00:00', '2018-07-16 12:00:00','some comments for testing', 'http://via.placeholder.com/350x150','2018-07-16 09:00:00','2018-07-16 09:00:00');
+
+INSERT INTO `sale_db`.`sale_event` (`useraccount_id`, `address`, `city`, `state`, `zip`, `date`, `start_time`, `end_time`,`comments`, `photo_url`,`createdAt`, `updatedAt`) VALUES ('1', '789 Anywhere Lane', 'Minneapolis', 'MN', '55406', '2018-07-16', '2018-07-16 09:00:00', '2018-07-16 12:00:00','some comments for testing', 'http://via.placeholder.com/350x150','2018-07-16 09:00:00','2018-07-16 09:00:00');
+
+INSERT INTO `sale_db`.`sale_event` (`useraccount_id`, `address`, `city`, `state`, `zip`, `date`, `start_time`, `end_time`,`comments`, `photo_url`,`createdAt`, `updatedAt`) VALUES ('1', '111 Anywhere Lane', 'Minneapolis', 'MN', '55406', '2018-07-16', '2018-07-16 09:00:00', '2018-07-16 12:00:00','some comments for testing', 'http://via.placeholder.com/350x150','2018-07-16 09:00:00','2018-07-16 09:00:00');
+
 
 # Sample Item Category Seed
-INSERT INTO `sale_db`.`item` (`sale_event_id`, `rank`, `category_name`) VALUES ('1', '8', 'Electronics');
+INSERT INTO `sale_db`.`item` (`sale_event_id`, `rank`, `category_name`,`createdAt`, `updatedAt`) VALUES ('1', '8', 'Electronics','2018-07-16 09:00:00','2018-07-16 09:00:00');
 
 # Sample Photo Seed
-INSERT INTO `sale_db`.`photo` (`item_id`, `url`) VALUES ('1', '/photos/12345678.png');
+INSERT INTO `sale_db`.`photo` (`item_id`, `url`,`createdAt`, `updatedAt`) VALUES ('1', '/photos/12345678.png','2018-07-16 09:00:00','2018-07-16 09:00:00');
